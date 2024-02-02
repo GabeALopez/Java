204a92ddf276b500785b0d1fa7ecc1f8dfe13c74
GabeALopez <109094200+GabeALopez@users.noreply.github.com>
2024-02-01 19:19:37 -0500

    Started display method

---
+++ src/Driver.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+/**
+ *
+ *    @author : Gabriel Lopez
+ *    Student Number : 0683
+ *    @date : 1/29/24
+ *    (PUT DESCRIPTION HERE)
+ */
+
+public class Driver {
+
+    public static void main(String[] args){
+
+        int[][] sudokuArr = {
+                {5, 3, 4, 0, 7, 8, 9, 1, 2},
+                {6, 7, 0, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 0, 7},
+                {8, 0, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 0, 3, 7, 9, 0},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 0, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 0, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9}
+        };
+
+
+
+        Scanner input = new Scanner(System.in);
+        String strInput = "";
+        int row = 0;
+        int column = 0;
+        int inputValue = 0;
+
+        Sudoku sudoku = new Sudoku();
+
+        while(true){
+
+            //TODO: Display Sudoku array here
+            sudoku.display(sudokuArr);
+
+
+            System.out.println("Continue Playing (Y/N)?");
+
+            strInput = input.next();
+
+            if(strInput != null) {
+
+               if(strInput.equals("N")){
+                   break;
+               }
+               if(strInput.equals("Y")){
+
+                   System.out.println("Enter row:");
+                   row = input.nextInt();
+
+                   System.out.println("Enter column:");
+                   column = input.nextInt();
+
+                   System.out.println("Enter value:");
+                   inputValue = input.nextInt();
+
+                   //TODO: Check value as well as if it fits in puzzle
+
+               }
+               else{
+                   System.out.println("Invalid input");
+                   System.out.println("Continue Playing (Y/N)?");
+
+               }
+
+            }
+            else {
+                System.out.println("Invalid input");
+            }
+
+
+        }
+
+    }
+}
---
+++ src/Sudoku.java
@@ -0,0 +1,66 @@
+public class Sudoku {
+
+    public Sudoku(){
+
+    }
+    public void display(int[][] arr){
+
+        int row = 9;
+        int column = 9;
+
+       for(int i = 0; i < row; i++) {
+           for(int j = 0; j < column;j++){
+
+               if((j+1) % 3 == 0){
+
+                  System.out.print(" | ");
+
+               }
+
+               System.out.print(arr[i][j] + " ");
+
+           }
+
+           if((i+1) % 3 == 0){
+
+               System.out.println();
+               System.out.print("------+-------+------");
+
+           }
+
+           System.out.println();
+
+
+       }
+
+        return;
+    }
+    public boolean isSolved(){ return true;}
+    public boolean isValueValid(String inputVal){
+
+        if(inputVal != null){
+
+           try {
+
+               int valHold = Integer.parseInt(inputVal);
+
+               if(1 <= valHold && valHold <= 9){
+
+                  return true;
+
+               }
+
+
+           }catch (NumberFormatException nfe){
+               return false;
+           }
+
+        }
+
+        return false;
+    }
+    public boolean isRowColumnValid(){ return true;}
+    public boolean isEntryCorrect(){ return true;}
+
+
+}
