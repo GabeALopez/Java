80e52817a660ca34ba58ce58df3191535e7bffa1
GabeALopez <109094200+GabeALopez@users.noreply.github.com>
2024-04-10 18:22:13 -0400

    Started Pokémon class in assignment 4

---
+++ Java/Assignment 4/src/Pokemon.java
@@ -0,0 +1,50 @@
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Pokemon implements Serializable {
+
+    //Private values
+    private UUID pokemonID;
+    private String pokemonType;
+    private String name;
+    private boolean checkedIn;
+
+    /**
+     *
+     * @param pPokemonType
+     * @param pName
+     * Pokemon constructor
+     */
+    Pokemon(String pPokemonType, String pName){
+
+        this.pokemonType = pPokemonType;
+        this.name = pName;
+
+
+    }
+
+    /**
+     * Change checked in to true or false
+     */
+    public void checkIn(){checkedIn = true;}
+    public void checkOut(){checkedIn = false;}
+
+    /**
+     * Getters
+     */
+    public boolean isCheckedIn() {
+        return checkedIn;
+    }
+
+    public UUID getPokemonID() {
+        return pokemonID;
+    }
+
+    public String getPokemonType() {
+        return pokemonType;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
